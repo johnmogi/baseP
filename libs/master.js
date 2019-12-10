30135eac6809efea6a746e85cdcecc910ad2284b
john mogi <john@accessibe.com>
2019-12-10 16:22:27 +0200

    promising

--- libs/master.js
+++ libs/master.js
@@ -2,97 +2,56 @@
 
 $(() => {
     let tempArr = []
-    const config = {
-        currencies: "https://api.coingecko.com/api/v3/coins/list",
-        currenciesCoin: "https://api.coingecko.com/api/v3/coins",
-        comparePrice: "https://min-api.cryptocompare.com/data"
-    }
-
-      // homepage getCoinsinCards
-
-    function getCoins() {
-        $.ajax({
-            method: "GET",
-            url: config.currenciesCoin,
-            error: err => alert(err.message),
-            success: currency => displayCoins(currency.slice(0, 9))
-        });
-    }
-    function displayCoins(currency) {
-        console.log(currency)
-         }
-
-    getCoins()
-    
-  
-
-    // live report getCoin into charts
-
-          
-    function getCoins2() {
-        $.ajax({
-            method: "GET",
-            url: config.currenciesCoin,
-            error: err => alert(err.message),
-            success: coinData => displayCoins2(coinData.slice(0, 9))
-        });
-    }
-    function displayCoins2(coinData) {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        // let tempArr = []
-for (const item in coinData) {
-    tempArr.push(coinData[item])
+    // // **(manifest documentation of other methods)**
+const config = {
+    currencies: "https://api.coingecko.com/api/v3/coins/list/",
+    currenciesCoin: "https://api.coingecko.com/api/v3/coins/",
+    comparePrice: "https://min-api.cryptocompare.com/data"
 }
-console.log(tempArr)
-
-var myChart = new Chart(ctx, {
-
-    type: 'bar',
-    data: {
-        
-        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        labels:[tempArr[0].name,tempArr[1].name],
-
-        datasets: [{
-            label: '# of Votes',
-            data:  [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+const api = {
+    retreiveCurrencies: function () {
+        return new Promise((resolve) => {
+            $.ajax({
+                url: config.currenciesCoin,
+                method: "GET",
+                success: function (currencies) {
+                    resolve(currencies)
+                },
+                error: function () {
+                    alert("error")
+                    // alert(xhr.status);
+                    // alert(thrownError);
                 }
-            }]
-        }
+            })
+        })
     }
-});
-         }
-
-    getCoins2()
+}
 
 
-// call chart. js
-// place data if retrieved into chart
-//
+api.retreiveCurrencies().
+then(res => {
+    saveTheCurrencies(res.slice(0, 8))
+})
+
+function saveTheCurrencies(result) {
+    tempArr.push(result)
+    console.log(result)
+    console.log(tempArr)
+    // draw(result)
+}
+
+// function getCoin(result) {
+//     console.log(result)
+//     console.log(tempArr)
+// }
+
+// getCoin()
+  // search in coins
+
+  function searchCoin(){
+    getCoins() 
+  }
+
 
 
 }); //RF
