cc02f707b6926a651930864b35d3da2d96447abb
john mogi <john@accessibe.com>
2019-12-11 16:39:22 +0200

    add temp

---
+++ libs/A/master copy2.js
@@ -0,0 +1,70 @@
+// call api and get data
+
+$(() => {
+    let tempArr = []
+    // // **(manifest documentation of other methods)**
+const config = {
+    currencies: "https://api.coingecko.com/api/v3/coins/list/",
+    currenciesCoin: "https://api.coingecko.com/api/v3/coins/",
+    comparePrice: "https://min-api.cryptocompare.com/data"
+}
+const api = {
+    retreiveCurrencies: function () {
+        return new Promise((resolve) => {
+            $.ajax({
+                url: config.currenciesCoin,
+                method: "GET",
+                success: function (currencies) {
+                    resolve(currencies)
+                },
+                error: function () {
+                    alert("error")
+                    // alert(xhr.status);
+                    // alert(thrownError);
+                },
+            })
+        })
+    }
+}
+
+// [{},{}]
+api.retreiveCurrencies().
+then(res => {
+    saveTheCurrencies(res.slice(0, 8))
+})
+
+function saveTheCurrencies(result) {
+    tempArr.push(result)
+    console.log(result)
+    // draw(result)
+}
+
+api.retreiveCurrencies().
+then(res => {
+    searchTheCurrencies(res.slice(0, 8))
+})
+
+function searchTheCurrencies(result) {
+    // showCoins https://api.coingecko.com/api/v3/coins?symbol/usd
+    for (const item of result) {
+        console.log(item + "/?symbol/usd")
+    }
+}
+
+// function searchForCoin(){ 
+// api.retreiveCurrencies().
+// then(res => {
+//     searchTheCurrencies(res.slice(0, 8))
+// });
+//  }
+// function searchTheCurrencies(result) {
+//     showCoins(result/ + /?symbol/usd)
+//     // draw(result)
+// }
+
+// function showCoins(filter){
+//     getAjaxData("https://restcountries.eu/rest/v2/" + filter, finalData => displayCountries(finalData));
+// }
+
+
+}); //RF
--- libs/master.js
+++ libs/master.js
@@ -1,58 +1,33 @@
 // call api and get data
 
 $(() => {
-    let tempArr = []
+    // let tempArr = []
     // // **(manifest documentation of other methods)**
 const config = {
     currencies: "https://api.coingecko.com/api/v3/coins/list/",
     currenciesCoin: "https://api.coingecko.com/api/v3/coins/",
     comparePrice: "https://min-api.cryptocompare.com/data"
 }
-const api = {
-    retreiveCurrencies: function () {
-        return new Promise((resolve) => {
-            $.ajax({
-                url: config.currenciesCoin,
-                method: "GET",
-                success: function (currencies) {
-                    resolve(currencies)
-                },
-                error: function () {
-                    alert("error")
-                    // alert(xhr.status);
-                    // alert(thrownError);
-                },
-            })
-        })
-    }
+
+$("#searchBut").click(() => {
+    console.log("?symbol/" + $("#searcher").val())
+    // showCoins("?symbol/" + $("#searcher").val())
+
+});
+
+function getAjaxData(url, callback) {
+    $.ajax({
+        method: "GET",
+        url: url,
+        error: err => alert("something went wrong, please try again ", err.message),
+        success: coins => callback(coins)
+    });
 }
 
-// [{},{}]
-api.retreiveCurrencies().
-then(res => {
-    saveTheCurrencies(res.slice(0, 8))
-})
-
-function saveTheCurrencies(result) {
-    tempArr.push(result)
-    console.log(result)
-    // draw(result)
+function showCoins(filter) {
+    getAjaxData(config.currenciesCoin + filter, finalData => displayCoins(finalData));
 }
-
-api.retreiveCurrencies().
-then(res => {
-    searchTheCurrencies(res.slice(0, 8))
-})
-
-function searchTheCurrencies(result) {
-    showCoins(result/ + /?symbol/usd)
-  
-    // draw(result)
+function displayCoins(coins) {
+    console.log(coins)
 }
-
-function showCoins(filter){
-    getAjaxData("https://restcountries.eu/rest/v2/" + filter, finalData => displayCountries(finalData));
-}
-
-
 }); //RF
