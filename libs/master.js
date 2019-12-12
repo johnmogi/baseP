65a3b19b00a30dee57c55e967a5873e1ab66fde8
john mogi <john@accessibe.com>
2019-12-13 01:48:56 +0200

    1

--- libs/master.js
+++ libs/master.js
@@ -1,8 +1,5 @@
-// call api and get data
-
 $(() => {
-    // let tempArr = []
-    // // **(manifest documentation of other methods)**
+
 const config = {
     currencies: "https://api.coingecko.com/api/v3/coins/list/",
     currenciesCoin: "https://api.coingecko.com/api/v3/coins/",
@@ -10,10 +7,19 @@ const config = {
 }
 
 $("#searchBut").click(() => {
-    console.log("?symbol/" + $("#searcher").val())
+    if (!$("#searcher").val()){
+        alert("please fill in the field")
+        return
+    }
+    else{  
+// console.log((config.currenciesCoin + "?symbol/" + $("#searcher").val()))
     // showCoins("?symbol/" + $("#searcher").val())
+    getAjaxData(config.currencies +"?symbol/" + $("#searcher").val(), finalData => displaySearchedCoins(finalData.slice(0, 9)));
 
+    // displayCoins(coins)
+    }
 });
+homeCoins()
 
 function getAjaxData(url, callback) {
     $.ajax({
@@ -23,11 +29,35 @@ function getAjaxData(url, callback) {
         success: coins => callback(coins)
     });
 }
-
-function showCoins(filter) {
-    getAjaxData(config.currenciesCoin + filter, finalData => displayCoins(finalData));
+function homeCoins() {
+    getAjaxData(config.currencies , finalData => displayCoins(finalData.slice(0, 9)));
+    
 }
+
 function displayCoins(coins) {
     console.log(coins)
+    $("#stage").empty();
+    let content = "";
+    for (const item of coins) {
+        const theCoin = `
+        ${item.name}             
+            `;
+        content += theCoin;
+    }
+    $("#stage").append(content);
 }
+
+function displaySearchedCoins(coins) {
+    console.log(coins)
+    $("#stage").empty();
+    let content = "";
+    for (const item of coins) {
+        const theCoin = `
+        ${item.name}             
+            `;
+        content += theCoin;
+    }
+    $("#stage").append(content);
+}
+
 }); //RF
